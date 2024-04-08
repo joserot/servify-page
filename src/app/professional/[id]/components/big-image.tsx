a319375a85ce1a2998bfaf0d444a73c68423097a
Jose Rotchen <jose.rotchen14@gmail.com>
2024-04-08 12:27:43 -0300

    galeria en perfil

---
+++ src/app/professional/[id]/components/big-image.tsx
@@ -0,0 +1,78 @@
+import { Button } from "@/components/ui/button";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faXmark,
+  faArrowRight,
+  faArrowLeft,
+} from "@fortawesome/free-solid-svg-icons";
+
+interface Props {
+  image: string;
+  setImage: (image: string) => void;
+  imagesList: string[];
+}
+
+export default function BigImage({ image, setImage, imagesList }: Props) {
+  if (!image) return null;
+
+  const handleClose = () => {
+    setImage("");
+  };
+
+  const handleGoRight = () => {
+    const imageActiveIndex = imagesList.findIndex((el) => {
+      return el === image;
+    });
+
+    const nextImage = imagesList[imageActiveIndex + 1];
+
+    if (nextImage) {
+      setImage(nextImage);
+    } else {
+      setImage(imagesList[0]);
+    }
+  };
+
+  const handleGoLeft = () => {
+    const imageActiveIndex = imagesList.findIndex((el) => {
+      return el === image;
+    });
+
+    const prevImage = imagesList[imageActiveIndex - 1];
+
+    if (prevImage) {
+      setImage(prevImage);
+    } else {
+      setImage(imagesList[imagesList.length - 1]);
+    }
+  };
+
+  return (
+    <div className="fixed inset-0 z-50 bg-black/80 flex items-center justify-center">
+      <Button
+        onClick={handleClose}
+        className="absolute top-4 right-4 md:top-20 md:right-20 flex items-center justify-center w-10 h-10 md:w-16 md:h-16 text-3xl"
+      >
+        <FontAwesomeIcon className="w-16" icon={faXmark} />
+      </Button>
+      <Button
+        onClick={handleGoRight}
+        className="absolute translate-y-[50%] right-4 md:right-20 flex items-center justify-center w-10 h-10 md:w-16 md:h-16 text-3xl"
+      >
+        <FontAwesomeIcon className="w-16" icon={faArrowRight} />
+      </Button>
+      <Button
+        onClick={handleGoLeft}
+        className="absolute translate-y-[50%] left-4 md:left-20 flex items-center justify-center w-10 h-10 md:w-16 md:h-16 text-3xl"
+      >
+        <FontAwesomeIcon className="w-16" icon={faArrowLeft} />
+      </Button>
+      <img
+        src={image}
+        width={500}
+        className="w-11/12 max-w-[700px] h-auto object-cover"
+      />
+    </div>
+  );
+}
--- src/app/professional/[id]/components/jobs.tsx
+++ src/app/professional/[id]/components/jobs.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import BigImage from "./big-image";
+
+import { useState } from "react";
+
 const images = [
   "/service-2.jpg",
   "/service-3.jpg",
@@ -20,19 +24,32 @@ const images = [
 ];
 
 export default function Jobs() {
+  const [image, setImage] = useState("");
+
+  const handleClick = (newImage: string) => {
+    console.log(newImage);
+
+    setImage(newImage);
+  };
+
   return (
-    <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 pt-10">
-      {images.map((image, i) => {
-        return (
-          <img
-            className="h-auto w-full object-cover rounded-sm"
-            key={i}
-            src={image}
-            width={200}
-            height={200}
-          />
-        );
-      })}
-    </div>
+    <>
+      <BigImage image={image} setImage={setImage} imagesList={images} />
+
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 pt-10">
+        {images.map((image, i) => {
+          return (
+            <img
+              onClick={() => handleClick(image)}
+              className="h-auto w-full object-cover rounded-sm cursor-pointer"
+              key={i}
+              src={image}
+              width={200}
+              height={200}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
