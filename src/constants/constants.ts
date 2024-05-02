912109a0d652384ff9006bab0744d8cb954eef81
Jose Rotchen <jose.rotchen14@gmail.com>
2024-05-02 20:12:02 -0300

    backend url on production

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,7 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://servify-page-backend-production.up.railway.app/api"
+    : "http://localhost:5000/api";
 
 const ACCESS_TOKEN_NAME = "servify-token";
 
