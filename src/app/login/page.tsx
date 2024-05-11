aeab40d4a9b206a0c74721b4671ba0e464b042fc
Jose Rotchen <jose.rotchen14@gmail.com>
2024-05-11 19:47:42 -0300

    banner

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,7 +14,10 @@ export default function LoginPage() {
         <Header />
       </div>
       <main className="flex">
-        <Banner />
+        <Banner
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac tincidunt elit. Pellentesque mollis velit mauris, id accumsan quam faucibus nec. Aenean cursus, nisi eget porta vestibulum, lacus risus interdum quam, in dignissim urna mi vitae libero. Phasellus tristique viverra laoreet"
+          images={["/banner-2.jpg", "/banner-1.jpg", "/banner-3.jpg"]}
+        />
         <div className="w-full py-20 md:py-5 md:w-1/2 md:min-h-[100vh]">
           <div className="mx-auto w-11/12 max-w-[500px] h-full flex flex-col justify-center items-center">
             <span className="block mb-4 text-3xl font-bold text-center">
--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,9 +1,57 @@
+"use client";
+
 import Link from "next/link";
 import { PROJECT_NAME } from "@/constants/constants";
 
-export default function Banner() {
+import { useState, useEffect } from "react";
+
+interface Props {
+  text?: string;
+  images?: string[];
+}
+
+export default function Banner({ text, images }: Props) {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  useEffect(() => {
+    if (!images || !images.length) return;
+
+    const intervalId = setInterval(() => {
+      setImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 6000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [images]);
+
+  const image = images && images.length ? images[imageIndex] : null;
+
+  const style = image
+    ? {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url(${image})`,
+      }
+    : {};
+
   return (
-    <div className="hidden md:flex w-1/2 min-h-[100vh] bg-primary p-10 flex-col justify-between">
+    <div
+      style={style}
+      className={`
+        hidden
+        md:flex
+        w-1/2
+        min-h-[100vh]
+        bg-gradient-to-r
+        from-blue-500
+        to-primary
+        bg-no-repeat
+        bg-cover
+        bg-center
+        p-10
+        flex-col
+        justify-between
+        transition-all`}
+    >
       <section>
         <Link
           href="/"
@@ -16,11 +64,7 @@ export default function Banner() {
           {PROJECT_NAME}
         </Link>
       </section>
-      <p className="text-xl text-white">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium id
-        orci vel varius. Mauris dictum urna id mi accumsan fringilla. Duis
-        ultrices justo vel odio elementum, at posuere tortor mollis.
-      </p>
+      {text ? <p className="text-xl text-white">{text}</p> : null}
     </div>
   );
 }
