5b6d97f336dd439f1b2f4178223ae96e278bc972
Jose Rotchen <jose.rotchen14@gmail.com>
2024-05-12 00:48:40 -0300

    categories modal location

--- src/app/(home)/components/service-card.tsx
+++ src/app/(home)/components/service-card.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+
+import ModalLocation from "@/components/modal-location";
+
+import { useState } from "react";
 
 interface Props {
   label: string;
@@ -10,14 +13,22 @@ interface Props {
 }
 
 export default function ServicesCard({ label, value, image }: Props) {
-  const router = useRouter();
+  const [isOpenModalLocations, setIsOpenModalLocations] = useState(false);
 
-  const handleClick = () => router.push(`/searcher?profession=${value}`);
+  const handleClick = () => {
+    setIsOpenModalLocations(true);
+  };
 
   return (
-    <div
-      onClick={handleClick}
-      className="
+    <>
+      <ModalLocation
+        isOpen={isOpenModalLocations}
+        service={value}
+        setOpen={setIsOpenModalLocations}
+      />
+      <div
+        onClick={handleClick}
+        className="
       cursor-pointer
       w-full
       sm:max-w-sm
@@ -31,17 +42,18 @@ export default function ServicesCard({ label, value, image }: Props) {
       border-gray-500
       dark:hover:shadow-slate-50
       dark:hover:shadow-sm "
-    >
-      <Image
-        src={image}
-        alt={label}
-        width={400}
-        height={400}
-        className="w-36 h-auto sm:w-full"
-      />
-      <div className="px-6 py-4 flex justify-center items-center">
-        <div className="text-md mb-2 sm:text-center">{label}</div>
+      >
+        <Image
+          src={image}
+          alt={label}
+          width={400}
+          height={400}
+          className="w-36 h-auto sm:w-full"
+        />
+        <div className="px-6 py-4 flex justify-center items-center">
+          <div className="text-md mb-2 sm:text-center">{label}</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
